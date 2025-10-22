f70fc28c0c1dbb0a743b8dc02bb630f453c3a919
Ashtu11 <anna.kalenyuku@Gmail.com>
2025-10-22 16:06:21 +0300

    Java_core_hw_3

--- Java/src/main/java/Java_core_h3/Java_core_hw3.java
+++ Java/src/main/java/Java_core_h3/Java_core_hw3.java
@@ -1,56 +1,66 @@
 package Java_core_h3;
+
 import java.util.Scanner;
 
 public class Java_core_hw3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[][] DaysOfTheWEEk = new String[7][2];
 
-        DaysOfTheWEEk[0][0] = "Sunday";
-        DaysOfTheWEEk[0][1] = "do home work";
+        String[][] schedule = new String[7][2];
 
-        DaysOfTheWEEk[1][0] = "Monday";
-        DaysOfTheWEEk[1][1] = "go to courses; watch a film";
-
-        DaysOfTheWEEk[2][0] = "Tuesday";
-        DaysOfTheWEEk[2][1] = "go to the gym";
-
-        DaysOfTheWEEk[3][0] = "Wednesday";
-        DaysOfTheWEEk[3][1] = "meet with friends";
-
-        DaysOfTheWEEk[4][0] = "Thursday";
-        DaysOfTheWEEk[4][1] = "study Java";
-
-        DaysOfTheWEEk[5][0] = "Friday";
-        DaysOfTheWEEk[5][1] = "clean the room";
-
-        DaysOfTheWEEk[6][0] = "Saturday";
-        DaysOfTheWEEk[6][1] = "rest and play games";
-
+        schedule[0][0] = "Sunday";
+        schedule[0][1] = "do homework";
+        schedule[1][0] = "Monday";
+        schedule[1][1] = "go to courses; watch a film";
+        schedule[2][0] = "Tuesday";
+        schedule[2][1] = "go to the gym";
+        schedule[3][0] = "Wednesday";
+        schedule[3][1] = "meet with friends";
+        schedule[4][0] = "Thursday";
+        schedule[4][1] = "read a book";
+        schedule[5][0] = "Friday";
+        schedule[5][1] = "do shopping";
+        schedule[6][0] = "Saturday";
+        schedule[6][1] = "rest and play games";
 
         while (true) {
             System.out.println("Please, input the day of the week:");
-            String input = scanner.nextLine().trim();
+            String input = scanner.nextLine().trim().toLowerCase();
 
-            if (input.equalsIgnoreCase("exit")) {
-                System.out.println("Exiting the program. Goodbye!");
+            if (input.equals("exit")) {
+                System.out.println("Goodbye!");
                 break;
             }
-            boolean found = false;
-            for (int i = 0; i < DaysOfTheWEEk.length; i++) {
-                if (DaysOfTheWEEk[i][0].equalsIgnoreCase(input)) {
-                    System.out.println("Your tasks for " + DaysOfTheWEEk[i][0] + ": " + DaysOfTheWEEk[i][1] + ".");
-                    found = true;
+
+            switch (input) {
+                case "sunday":
+                    System.out.println("Your tasks for Sunday: " + schedule[0][1]);
                     break;
-                }
-            }
-
-
-            if (!found) {
-                System.out.println("Sorry, I don't understand you, please try again.");
+                case "monday":
+                    System.out.println("Your tasks for Monday: " + schedule[1][1]);
+                    break;
+                case "tuesday":
+                    System.out.println("Your tasks for Tuesday: " + schedule[2][1]);
+                    break;
+                case "wednesday":
+                    System.out.println("Your tasks for Wednesday: " + schedule[3][1]);
+                    break;
+                case "thursday":
+                    System.out.println("Your tasks for Thursday: " + schedule[4][1]);
+                    break;
+                case "friday":
+                    System.out.println("Your tasks for Friday: " + schedule[5][1]);
+                    break;
+                case "saturday":
+                    System.out.println("Your tasks for Saturday: " + schedule[6][1]);
+                    break;
+                default:
+                    System.out.println("Sorry, I don't understand you, please try again.");
             }
         }
 
         scanner.close();
     }
+
 }
+
