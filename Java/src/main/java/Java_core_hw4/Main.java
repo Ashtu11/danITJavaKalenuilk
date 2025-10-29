250a69acb246195af689392704b960e0ca0f1458
Ashtu11 <anna.kalenyuku@Gmail.com>
2025-10-29 11:49:40 +0200

    ProjectFamily

---
+++ Java/src/main/java/Java_core_hw4/Main.java
@@ -0,0 +1,58 @@
+package Java_core_hw4;
+
+public class Main {
+    public static void main(String[] args) {
+        Pet dog = new Pet("Rock", "dog", 75, 5, new String[]{"eat", "drink", "sleep"});
+        Pet cat = new Pet("", "Murka", 45, 2, new String[]{"scratch", "sleep"});
+
+
+        Human mother1 = new Human("Jane", "Karleone", 1975, (byte) 80);
+        Human father1 = new Human("Vito", "Karleone", 1970, (byte) 80);
+
+        Human mother2 = new Human("Anna", "Smith", 1980, (byte) 90);
+        Human father2 = new Human("Michael", "Karleone", 1977, (byte) 90);
+
+
+        Family family1 = new Family(mother1, father1);
+        family1.setPet(dog);
+
+        Family family2 = new Family(mother2, father2);
+        family2.setPet(cat);
+
+
+        Human child1 = new Human("Michael", "Karleone", 2000);
+        Human child2 = new Human("Sophie", "Smith", 2005);
+
+
+        family1.addChild(child1);
+        family2.addChild(child2);
+
+
+        System.out.println(" Сім'я 1 ");
+        System.out.println(family1);
+        System.out.println("Кількість членів сім'ї: " + family1.countFamily());
+        child1.greetPet();
+        child1.describePet();
+
+        System.out.println("\nСім'я 2 ");
+        System.out.println(family2);
+        System.out.println("Кількість членів сім'ї: " + family2.countFamily());
+        child2.greetPet();
+        child2.describePet();
+
+
+        System.out.println("\nЧи однакові сім'ї? " + family1.equals(family2));
+        System.out.println("Чи однакові батьки? " + father1.equals(father2));
+        System.out.println("Чи однакові тварини? " + dog.equals(cat));
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Java/src/main/java/Java_core_hw4/Pet.java
@@ -0,0 +1,106 @@
+package Java_core_hw4;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Pet {
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
+
+    public void eat() {
+        System.out.println("Я їм.");
+
+    }
+
+    public void respond() {
+        System.out.println("Привіт, хазяїн. Я " + this.nickname + "Я скучив!");
+    }
+
+    public void foul() {
+        System.out.println("Потрібно добре замести сліди...");
+
+    }
+
+    public Pet() {
+
+    }
+
+    public Pet(String nickname, String species) {
+        this.nickname = nickname;
+        this.species = species;
+    }
+
+    public Pet(String nickname, String species, int trickLevel, int age, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.habits = habits;
+        this.trickLevel = trickLevel;
+
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    @Override
+    public String toString() {
+        return species + "{" +
+                ", nickname='" + nickname + '\'' +
+                ", age=" + age +
+                ", trickLevel=" + trickLevel +
+                ", habits=" + Arrays.toString(habits) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pet pet = (Pet) o;
+        return age == pet.age && trickLevel == pet.trickLevel;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, trickLevel);
+    }
+}
