d37a82d0a357e39ab0f1940fea63fe7d4a82e38a
Ashtu11 <anna.kalenyuku@Gmail.com>
2025-10-31 19:52:15 +0200

    HomeWork5

---
+++ Java/src/main/java/Java_core_hw5/Family.java
@@ -0,0 +1,84 @@
+package Java_core_hw5;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Family {
+    private Human Mother;
+    private Human Father;
+    private Human[] Children;
+    private Pet pet;
+
+    public void setMother(Human mother) {
+        Mother = mother;
+    }
+
+    public void setFather(Human father) {
+        Father = father;
+    }
+
+    public void setChildren(Human[] children) {
+        Children = children;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Human getMother() {
+        return Mother;
+    }
+
+    public Human getFather() {
+        return Father;
+    }
+
+    public Human[] getChildren() {
+        return Children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public Family(Human mother, Human father) {
+        this.Mother = mother;
+        this.Father = father;
+        this.Children = new Human[0];
+        mother.setFamily(this);
+        father.setFamily(this);
+
+    }
+    public void addChild(Human child) {
+        Human[] newChildren = Arrays.copyOf(Children, Children.length + 1);
+        newChildren[Children.length] = child;
+        Children = newChildren;
+        child.setFamily(this);
+    }
+    public int countFamily() {
+        return 2 + Children.length;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "Mother=" + Mother +
+                ", Father=" + Father +
+                ", Children=" + Arrays.toString(Children) +
+                ", pet=" + pet +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return Objects.equals(Mother, family.Mother) && Objects.equals(Father, family.Father) && Objects.equals(pet, family.pet);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Mother, Father, pet);
+    }
+}
---
+++ Java/src/main/java/Java_core_hw5/Human.java
@@ -0,0 +1,108 @@
+package Java_core_hw5;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Human {
+    private String name;
+    private String surname;
+    private int year;
+    private byte iq;
+    private Family family;
+    private String[][] schedule;
+
+    public Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+
+    public Human(String name, String surname, int year, byte iq) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+    }
+
+    public Human(String name, String surname, int year, Human Mother, Human Father) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+    public Family getFamily() {
+        return family;
+    }
+
+    public void setFamily(Family family) {
+        this.family = family;
+    }
+
+
+    public Human() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getFullName() {
+        return this.name + " " + this.surname;
+    }
+
+
+    public void greetPet() {
+        System.out.println("Привіт, " + this.family.getPet().getNickname() + "!");
+    }
+
+    public void describe() {
+        Human father = this.family.getFather();
+        Human mother = this.family.getMother();
+        System.out.println("Ім'я: " + name);
+        System.out.println("Прізвище: " + surname);
+        System.out.println("Рік народження: " + year);
+        System.out.println("IQ: " + iq);
+        if (mother != null)
+            System.out.println("Мама: " + mother.getFullName());
+        if (father != null)
+            System.out.println("Тато: " + father.getFullName());
+    }
+
+    public void describePet() {
+        Pet pet = this.family.getPet();
+        System.out.println("У мене є " +
+                pet.getSpecies() +
+                " їй " + pet.getAge() +
+                " років, він " +
+                (pet.getTrickLevel() > 50 ? "дуже хитрий" : "майже не хитрий"));
+        System.out.println(pet.getNickname());
+
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", year=" + year +
+                ", iq=" + iq +
+                ", schedule=" + Arrays.toString(schedule) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return year == human.year && iq == human.iq;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(year, iq);
+    }
+}
